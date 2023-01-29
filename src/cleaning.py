bf9b5d452d2c4930baccb1cdda6bdda7f78db8b0
Andres Castro <castroandres92@gmail.com>
2023-01-29 13:31:26 +0100

    Commit at:

--- src/cleaning.py
+++ src/cleaning.py
@@ -4,3 +4,10 @@ def delete_columns (df, columns):
     df.drop(columns = columns, inplace=True)
     print("Deleted columns: ", list(columns))
     return
+
+def compare_columns (df, columns):
+    lengths_list = []
+    for i in columns:
+        lengths_list.append(len(df[i]))
+    max_len = max(lengths_list)
+    return max_len

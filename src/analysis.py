1f54ee4736123cd28edaee8e3c615a0a1b4f2872
Andres Castro <castroandres92@gmail.com>
2023-01-31 00:28:01 +0100

    Commit at:

---
+++ src/analysis.py
@@ -0,0 +1,9 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+
+sns.set_context("poster")
+sns.set(rc={"figure.figsize": (16.,11.)})
+sns.set_style("whitegrid")
--- src/cleaning.py
+++ src/cleaning.py
@@ -1,7 +1,9 @@
 import pandas as pd
+import numpy as np
 import warnings
 from pandas.core.common import SettingWithCopyWarning   #I import this library and setting to be able to hide warnings that show me information I do not need.
 from IPython.display import display                     #I was getting an error claiming display was not defined. I import this to stop that error even though everything worked.
+import re
 
 def delete_columns (df, columns):
     df.drop(columns = columns, inplace=True)
@@ -86,3 +88,78 @@ def different_value_columns (df, columns):
 
     print(f"Columns differ in the following {counts} values")
     return differents[columns]
+
+
+def extract_date(df, column):
+    """"
+    This function takes a DataFrame and a specified column and looks for date data in that column. It looks for date data specified in the following format yyyy.mm.dd.
+    The regex is defined in such a way that it ignores anything else in the string before or after the match.
+    """
+    df["date_p"] = df[column].apply(lambda x: re.search('\d{4}\.\d{2}\.\d{2}', x).group(0) if re.search('\d{4}\.\d{2}\.\d{2}', x) else " ")
+    df["date_p"] = pd.to_datetime(df["date_p"], format='%Y.%m.%d', errors='coerce')
+    return df
+
+
+def extract_activity(df, column):
+    df["activity_p"] = df[column].astype(str)
+    df["activity_p"] = df["activity_p"].apply(lambda x: 'swimming' if re.search('swim|bath|div|snork|feed|wading|shark|air|wash|walk|floa|tread|water|play|splas|ocea|skii', x, re.IGNORECASE)
+                        else 'surfing' if re.search('surf|board|surfi|padl|ding', x, re.IGNORECASE)
+                        else 'fishing' if re.search('fish|stan', x, re.IGNORECASE)
+                        else 'sailing' if re.search('ship|boat|sail|kaya|cano|adrift|pad|sunk|yatch|yacht|row|sink|sea|fell|fall|cruis', x, re.IGNORECASE)
+                        else 'others')
+    return df
+
+
+def process_time_of_day(df, column):
+    
+    
+    df["int_time_of_day"] = df[column].astype(str).apply(lambda x: re.search('(\d{2})h', x).group(1) if re.search('(\d{2})h', x) else None)
+    df["cat_time_of_day"] = df[column].astype(str).apply(lambda x: 'afternoon' if re.search('noon|sunset', x, re.IGNORECASE)
+                        else 'morning' if re.search('morning|day|sunrise', x, re.IGNORECASE)
+                        else 'night' if re.search('night|dark|dusk', x, re.IGNORECASE)
+
+                        
+ #                       else "morning" if 6 <= int(re.search('(\d{2})h', x) <= 12)
+ #                       else "afternoon" if 12 < int(re.search('(\d{2})h', x) <= 19)
+ #                       else "night" if int(re.search('(\d{2})h', x) > 19 or int(re.search('(\d{2})h', x) < 6))
+                        
+
+
+                       else None)
+    return df
+    
+
+def correct_age(df, column):
+    df["age_p"] = df[column]
+    df["age_p"] = df["age_p"].replace(np.nan," ")
+    df["age_p"] = df["age_p"].astype(str)
+    df["age_p"] = df["age_p"].apply(lambda x: re.search('(\d{1,2})', x).group(0) if re.search('(\d{1,2})', x) else "")
+    return df
+
+
+def correct_fatality(df, column):
+    df["fatal_p"] = df[column]
+    df["fatal_p"] = df["fatal_p"].replace(np.nan," ")
+    df["fatal_p"] = df["fatal_p"].astype(str)
+    df["fatal_p"] = df["fatal_p"].apply(lambda x: re.search("(^[N,Y]$)", x.upper()).group(0) if re.search("(^[N,Y]$)", x.upper()) else "")
+    return df
+
+
+def process_species(df, column):
+    df["species_p"] = df[column].astype(str)
+    df["species_p"] = df["species_p"].apply(lambda x: 'White Shark' if re.search('white', x, re.IGNORECASE)
+                        else 'Tiger Shark' if re.search('tiger', x, re.IGNORECASE)
+                        else 'Bull Shark' if re.search('bull', x, re.IGNORECASE)
+                        else 'Wobbegong Shark' if re.search('wobbegong', x, re.IGNORECASE)
+                        else 'Hammerhead Shark' if re.search('ham', x, re.IGNORECASE)
+                        else 'No shark involved' if re.search('confirma|invalid|quest', x, re.IGNORECASE)
+                        else 'Uncomfirmed Species')
+    
+    return df
+
+
+
+
+
+
+
